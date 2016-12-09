f25bc9650d352d464fa6c7e5fc76dae64d07d1bd
Chandler Waugh <chandler@gbpackerfans.com>
2016-12-08 18:43:29 -0700

    Modified table to have questionNumber rather than highScore and the table to not allow duplicated usernames.

--- beegameinfo.sql
+++ beegameinfo.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `beegameinfo` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `beegameinfo`;
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.12, for Win32 (AMD64)
 --
 -- Host: localhost    Database: beegameinfo
 -- ------------------------------------------------------
@@ -29,11 +27,12 @@ CREATE TABLE `gamedata` (
   `username` varchar(16) DEFAULT NULL,
   `password` varchar(30) DEFAULT NULL,
   `teacher` char(1) DEFAULT NULL,
-  `highScore` int(11) DEFAULT NULL,
+  `questionNumber` int(11) DEFAULT NULL,
   `totalScore` int(11) DEFAULT NULL,
   `currentLevel` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,6 +41,7 @@ CREATE TABLE `gamedata` (
 
 LOCK TABLES `gamedata` WRITE;
 /*!40000 ALTER TABLE `gamedata` DISABLE KEYS */;
+INSERT INTO `gamedata` VALUES (1,'Henry','123456','N',0,0,0),(7,'Tom','1','Y',0,0,0),(8,'Bob','2','Y',0,0,0),(9,'Mike','3','N',0,0,0),(10,'teacher1','1','Y',0,0,0);
 /*!40000 ALTER TABLE `gamedata` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-12-06 21:32:50
+-- Dump completed on 2016-12-08 18:42:47
